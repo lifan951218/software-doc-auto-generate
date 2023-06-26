334e12406823c879cb601a59ce8ec7bfe6d19893
lifan <874508852@qq.com>
2023-06-26 17:15:52 +0800

    add: chatgpt可以初步提问并输出回答到doc

---
+++ src/main/java/com/example/softwaredocautogenerate/SoftwareDocAutoGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwaredocautogenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDocAutoGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareDocAutoGenerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softwaredocautogenerate/SoftwareDocAutoGenerateApplicationTests.java
@@ -0,0 +1,283 @@
+package com.example.softwaredocautogenerate;
+
+import com.plexpt.chatgpt.ChatGPT;
+import com.plexpt.chatgpt.util.Proxys;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileOutputStream;
+import java.net.Proxy;
+
+@SpringBootTest
+class SoftwareDocAutoGenerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        Proxy proxy = Proxys.http("127.0.0.1", 7890);
+
+        ChatGPT chatGPT = ChatGPT.builder()
+//                .apiKey("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")
+                .apiKey("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")
+                .timeout(900)
+                .proxy(proxy)
+                .apiHost("https://api.openai.com/") //代理地址
+                .build()
+                .init();
+        String code = """
+                <template>
+                  <div class="container">
+                    <div class="add-appointment">
+                      <h3>精彩回放</h3>
+                    </div>
+                    <el-form :inline="true" :model="queryForm">
+                      <el-form-item label="直播名称">
+                        <el-input v-model.trim="queryForm.直播" placeholder="请输入直播名称"></el-input>
+                      </el-form-item>
+                      <el-form-item label="回放日期">
+                        <el-date-picker format="YYYY/MM/DD" value-format="YYYY/MM/DD" v-model="queryForm.date" type="date" placeholder="选择直播日期"></el-date-picker>
+                      </el-form-item>
+                      <el-form-item label="">
+                        <el-button type="primary" @click="handleQuery">搜索</el-button>
+                <!--        <el-button type="primary" @click="handleExport">导出</el-button>-->
+                                
+                      </el-form-item>
+                    </el-form>
+                    <el-table :data="filteredSalesData">
+                      <el-table-column type="selection" width="55"></el-table-column>
+                      <el-table-column prop="date" label="直播日期"></el-table-column>
+                      <el-table-column prop="直播" label="直播"></el-table-column>
+                      <el-table-column prop="amount" label="观看人数"></el-table-column>
+                      <el-table-column label="操作" width="280">
+                        <template #default="{row}">
+                          <!-- 编辑直播 -->
+                          <!--          <el-button type="primary" size="small" @click="editAppointment(row)">编辑</el-button>-->
+                          <el-button  type="primary" size="small" @click="editAppointment(row)">进入回放</el-button>
+                                
+                          <!-- 取消直播 -->
+                          <!--          <el-button v-if="row.status === '直播中'" type="danger" size="small" @click="cancelAppointment(row)">关闭直播</el-button>-->
+                        </template>
+                      </el-table-column>
+                      <!--      <el-table-column prop="cost" label="成本"></el-table-column>-->
+                      <!--      <el-table-column prop="profit" label="利润"></el-table-column>-->
+                    </el-table>
+                    <el-pagination
+                        style="margin-top:15px"
+                        background
+                        layout="prev, pager, next"
+                        :total="salesData.length"
+                        :page-size="pageSize"
+                        @current-change="handleCurrentChange"
+                    ></el-pagination>
+                  </div>
+                                
+                </template>
+                <script setup>
+                import {computed, ref} from 'vue';
+                import * as echarts from 'echarts';
+                import imgurl from '../assets/img/zhibo.jpeg';
+                                
+                const message = ref('first');
+                                
+                const salesData = ref([]);
+                                
+                for (let i = 0; i < 100; i++) {
+                  salesData.value.push({
+                    date: `2022/01/${i + 1}`,
+                    直播: `直播 ${i + 1}`,
+                    amount: Math.floor(Math.random() * 100),
+                    cost: Math.floor(Math.random() * 5000),
+                    profit: null
+                  });
+                  // 计算利润
+                  salesData.value[i].profit = salesData.value[i].amount - salesData.value[i].cost;
+                }
+                const pageSize = ref(10); // 每页显示的数据量
+                const currentPage = ref(1); // 当前页码
+                                
+                const queryForm = ref({
+                  直播: '',
+                  date: ''
+                });
+                                
+                const filteredSalesData = computed(() => {
+                  let data = JSON.parse(JSON.stringify(salesData.value));
+                  console.log("date:", queryForm.value.date)
+                  if (queryForm.value.直播) {
+                    data = data.filter(item => item.直播.includes(queryForm.value.直播));
+                  }
+                  if (queryForm.value.date) {
+                    data = data.filter(item => item.date === queryForm.value.date);
+                  }
+                  const start = (currentPage.value - 1) * pageSize.value;
+                  const end = start + pageSize.value;
+                  return data.slice(start, end);
+                });
+                                
+                                
+                const handleCurrentChange = (page) => {
+                  currentPage.value = page;
+                };
+                const handleQuery = () => {
+                  currentPage.value = 1; // 切换查询条件后回到第一页
+                }
+                                
+                const handleExport = () => {
+                  // 获取表格数据
+                  const tableData = JSON.parse(JSON.stringify(filteredSalesData.value));
+                  // 移除id字段
+                  tableData.forEach(item => {
+                    delete item.id;
+                  });
+                  // 导出xlsx文件
+                  import('xlsx').then(xlsx => {
+                    const worksheet = xlsx.utils.json_to_sheet(tableData);
+                    const workbook = xlsx.utils.book_new();
+                    xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+                    xlsx.writeFile(workbook, 'sales_data.xlsx');
+                  });
+                };
+                                
+                const tabChange = (value) => {
+                  if(value === 'second') {
+                    setTimeout(() => {
+                      initLineChart();
+                      initBarChart();
+                      initPieChart();
+                      initScatterChart();
+                    });
+                  }
+                }
+                                
+                const lineChart = ref(null);
+                const barChart = ref(null);
+                const pieChart = ref(null);
+                const scatterChart = ref(null);
+                                
+                const initLineChart = () => {
+                  const chart = echarts.init(lineChart.value);
+                  chart.setOption({
+                    xAxis: {
+                      type: 'category',
+                      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+                    },
+                    yAxis: {
+                      type: 'value'
+                    },
+                    series: [{
+                      data: [1200, 1500, 1800, 2000, 2200, 2400],
+                      type: 'line'
+                    }]
+                  });
+                };
+                                
+                const initBarChart = () => {
+                  const chart = echarts.init(barChart.value);
+                  chart.setOption({
+                    xAxis: {
+                      type: 'category',
+                      data: ['北京', '上海', '广州', '深圳', '杭州']
+                    },
+                    yAxis: {
+                      type: 'value'
+                    },
+                    series: [{
+                      data: [1200, 800, 500, 300, 200],
+                      type: 'bar'
+                    }]
+                  });
+                };
+                                
+                const initPieChart = () => {
+                  const chart = echarts.init(pieChart.value);
+                  chart.setOption({
+                    series: [{
+                      type: 'pie',
+                      data: [
+                        { name: '搜索引擎', value: 335 },
+                        { name: '直接访问', value: 310 },
+                        { name: '邮件营销', value: 234 },
+                        { name: '联盟广告', value: 135 },
+                        { name: '视频广告', value: 1548 }
+                      ]
+                    }]
+                  });
+                };
+                                
+                const initScatterChart = () => {
+                  const chart = echarts.init(scatterChart.value);
+                  chart.setOption({
+                    xAxis: {
+                      type: 'value'
+                    },
+                    yAxis: {
+                      type: 'value'
+                    },
+                    series: [{
+                      type: 'scatter',
+                      symbolSize: 10,
+                      data: [
+                        [10, 20],
+                        [30, 40],
+                        [50, 60],
+                        [70, 80],
+                        [90, 100]
+                      ]
+                    }]
+                  });
+                };
+                                
+                </script>
+                                
+                <style scoped>
+                .chart-container {
+                  display: flex;
+                  flex-wrap: wrap;
+                }
+                                
+                .chart-item {
+                  width: 50%;
+                  padding: 20px;
+                  box-sizing: border-box;
+                }
+                                
+                .add-appointment {
+                  margin-bottom: 20px;
+                }
+                                
+                .chart {
+                  height: 300px;
+                  border: 1px solid #ccc;
+                }
+                </style>
+                """;
+        String answer = chatGPT.chat("我给你一份样例代码，是用vue3以及element实现的，请根据样例代码生成一份关于角色管理的代码:\n"+code);
+
+        System.out.println(answer);
+        // 创建一个新的文档
+        XWPFDocument document = new XWPFDocument();
+
+        // 创建段落
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun run = paragraph.createRun();
+        run.setText(answer);
+
+        // 保存文档
+        try {
+            FileOutputStream out = new FileOutputStream("output2.docx");
+            document.write(out);
+            out.close();
+            System.out.println("Word文档生成成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
